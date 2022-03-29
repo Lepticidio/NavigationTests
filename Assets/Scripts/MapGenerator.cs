fe668f9614b9b94b1bda02bc25567f87465f5eb5
Lepticidio <mundaling@gmail.com>
2022-03-29 16:38:31 +0200

    Basic map generation

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -4,92 +4,83 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
+
+    float m_fInverseWidth, m_fInverseHeight, m_fCellSizeFactor = 0.05f;
+    public int m_iWidth, m_iHeight, m_iDepth;
+    public float m_fScale, m_fOffsetX, m_fOffsetY, m_fSeaLevel, m_fSeaDepth;
+    public Terrain m_oTerr;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GenerateTerrain();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
-    void InitializeLevel()
+    void PreparateTerrain()
     {
-        world = GameObject.Find("Data").GetComponent<World>();
-        music = world.GetComponent<MusicControl>();
-        biome = world.currentBiome;
-        GetComponent<MaterialController>().Initialize(world);
+        //m_oTerr.terrainData.SetHeights(0, 0, heights);
+    }
+
+    void GenerateTerrain()
+    {
 
 
-        surface = GetComponent<NavMeshSurface>();
-        offsetX = Random.Range(0f, 99999f);
-        offsetZ = Random.Range(0f, 99999f);
+        m_oTerr.terrainData.size = new Vector3(m_iWidth * m_fCellSizeFactor, m_iDepth * m_fCellSizeFactor, m_iHeight * m_fCellSizeFactor);
+        m_oTerr.gameObject.transform.position = new Vector3(-m_oTerr.terrainData.size.x * 0.5f, -m_oTerr.terrainData.size.y * m_fSeaLevel, -m_oTerr.terrainData.size.z * 0.5f);
 
-        sealevel = biome.sealevel;
+        float[,] fTempHeightMap = new float[2049, 2049];
 
-        TerrainData terrainData = m_oSandyEarth;
-        if (biome.humidity >= 300 && biome.sealevel < 1)
+        float fHeightMapWidth = m_oTerr.terrainData.heightmapResolution;
+        float fInverseHmWidth = 1f / (float)(fHeightMapWidth - 1);
+
+        m_fInverseWidth = 1f / (float)m_iWidth;
+        m_fInverseHeight = 1f / (float)m_iHeight;
+
+
+        for (int i = 0; i < m_iWidth; i++)
         {
-            scale *= 2;
-        }
-        seaDepth = seaDepth * sealevel;
-
-        if (biome.temperature < 5)
-        {
-            terrainData = m_oSandyTundra;
-        }
-        else
-        {
-            if (biome.humidity > 200)
+            for (int j = 0; j < m_iHeight; j++)
             {
-                terrainData = m_oSandyMud;
-            }
-            else if (biome.temperature < 10)
-            {
-                terrainData = m_oSnowyEarth;
-            }
-            else if (biome.humidity > 100)
-            {
-                terrainData = m_oMossyEarth;
+               float fAltitude = CalculateAltitude(i, j) * 0.8f + 0.1f;
+
+                int remainderI = (int)((float)i % (m_iWidth * fInverseHmWidth));
+                int remainderJ = (int)((float)j % (m_iHeight * fInverseHmWidth));
+                if (fAltitude < m_fSeaLevel)
+                {
+                    if (remainderI == 0 && remainderJ == 0)
+                    {
+                        if (fAltitude > m_fSeaLevel - m_fSeaDepth)
+                        {
+                            fTempHeightMap[j, i] = fAltitude;
+                        }
+                        else
+                        {
+                            fTempHeightMap[j, i] = m_fSeaLevel - m_fSeaDepth;
+                        }
+                    }
+                }
+                else
+                {
+                    if (remainderI == 0 && remainderJ == 0)
+                    {
+                        fTempHeightMap[j, i] = m_fSeaLevel;
+                    }
+                }
             }
         }
-
-        terrainWidth = terrainCells.GetLength(0);
-        terrainHeight = terrainCells.GetLength(1);
-
-        GameObject terrGO = Terrain.CreateTerrainGameObject(terrainData);
-        terr = terrGO.GetComponent<Terrain>();
-        terrGO.name = "Terrain";
-        terrGO.layer = 8;
-        terr.materialType = Terrain.MaterialType.Custom;
-        terr.materialTemplate = terrainMaterial;
-        terr.terrainData.size = new Vector3(width * CellConversor.factor, depth * CellConversor.factor, height * CellConversor.factor);
-        terrGO.transform.position = new Vector3(-terr.terrainData.size.x * 0.5f, -terr.terrainData.size.y * sealevel, -terr.terrainData.size.z * 0.5f);
-        hmWidth = terr.terrainData.heightmapWidth;
-        terr.terrainData.SetHeights(0, 0, heights);
-
-        itemData = GameObject.Find("Data").GetComponent<ItemData>();
-        plants = GameObject.Find("Plants").transform;
-        decorations = GameObject.Find("Decorations").transform;
-        foods = GameObject.Find("Foods").transform;
-        animals = GameObject.Find("Animals").transform;
-        rockGenerator = GetComponent<RockGenerator>();
-
-        grid.depth = depth;
-        grid.sizeY = terr.terrainData.size.y;
-        grid.sealevel = sealevel;
-        grid.seaDepth = seaDepth;
-
+        m_oTerr.terrainData.SetHeights(0, 0, fTempHeightMap);
     }
 
     float CalculateAltitude(int _iX, int _iY)
     {
 
-        float fXPerl = (float)_iX * inverseWidth * scale + offsetX;
-        float fYPerl = (float)_iY * inverseHeight * scale + offsetZ;
+        float fXPerl = (float)_iX * m_fInverseWidth * m_fScale + m_fOffsetX;
+        float fYPerl = (float)_iY * m_fInverseHeight * m_fScale + m_fOffsetY;
         float fPerlinNoise = Mathf.PerlinNoise(fXPerl, fYPerl);
         return fPerlinNoise;
     }
