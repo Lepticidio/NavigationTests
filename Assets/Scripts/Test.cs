5477afff882085236c8f21028b2c797c981e8e13
Lepticidio <mundaling@gmail.com>
2022-05-23 17:04:36 +0200

    Can measure accurately the time that it takes to do something

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System;
+using System.Runtime.InteropServices;
 [CreateAssetMenu(menuName = "Test")]
 public class Test : ScriptableObject
 {
@@ -11,11 +12,15 @@ public class Test : ScriptableObject
     public List<NodeMap> m_tNodeMaps = new List<NodeMap>();
     public NodeMap m_oLastNodeMap;
 
+
     public IEnumerator RunTests(MapGenerator _oMapGen, Agent _oAgent, Goal _oGoal)
     {
         for (int i = 0; i < m_iMapNumber; i++)
         {
+            DateTime oBeforeGenerateMap = HighResolutionDateTime.UtcNow;
             _oMapGen.GenerateMap(m_oMapType);
+            DateTime oAfterGenerateMap = HighResolutionDateTime.UtcNow;
+            Debug.Log("Map " + i +"-Time to generate map: " + (oAfterGenerateMap - oBeforeGenerateMap).TotalMilliseconds);
             yield return new WaitForSeconds(0.05f);
             for (int j = 0; j < m_tNodeMaps.Count; j++)
             {
@@ -23,11 +28,17 @@ public class Test : ScriptableObject
                 m_oLastNodeMap = oNodeMap;
                 for (int k = 0; k < m_iRepetitionsPerMap; k++)
                 {
+                    DateTime oBeforeGenerateNodes = HighResolutionDateTime.UtcNow;
                     oNodeMap.GenerateMap(m_oMapType);
-                    _oGoal.RandomPosition(m_oMapType, oNodeMap);
-                    _oAgent.StartPath(m_oMapType, oNodeMap);
+                    DateTime oAfterGenerateNodes = HighResolutionDateTime.UtcNow;
+                    Debug.Log("Map " + i + " algorithm "+ j + " iteration " + k+ "-Time to generate nodes: " + (oAfterGenerateNodes - oBeforeGenerateNodes).TotalMilliseconds);
 
-                    Debug.Log("map " + i + " node map "+ j + " iteration " + k);
+                    _oGoal.RandomPosition(m_oMapType, oNodeMap);
+                    DateTime oBeforeGeneratePath = HighResolutionDateTime.UtcNow;
+                    _oAgent.StartPath(m_oMapType, oNodeMap);
+                    DateTime oAfterGeneratePath = HighResolutionDateTime.UtcNow;
+                    Debug.Log("Map " + i + " algorithm " + j + " iteration " + k + "-Time to generate path: " + (oAfterGeneratePath - oBeforeGeneratePath).TotalMilliseconds);
+
                     yield return new WaitForSeconds(5f);
                 }
             }
